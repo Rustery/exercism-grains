fd7b78acb055474b3075e847665210d0028d7d09
Rustam <rustery@gmail.com>
2020-06-16 13:39:05 +0300

    clear loops and remove math module

--- grains.go
+++ grains.go
@@ -3,7 +3,6 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Square — get how many grains were on a given square of chessboard
@@ -11,15 +10,10 @@ func Square(input int) (uint64, error) {
 	if input < 1 || input > 64 {
 		return 0, errors.New("square should be greater than 0 and less or equal 64")
 	}
-	return uint64(math.Pow(2, float64(input-1))), nil
+	return 1 << (input - 1), nil
 }
 
 // Total — get the total number of grains on the chessboard
 func Total() uint64 {
-	var total uint64
-	for i := 1; i <= 64; i++ {
-		square, _ := Square(i)
-		total += square
-	}
-	return total
+	return 1<<64 - 1
 }
